d68c7a9e23172c35e2ea32a14ef59607edd2acb0
kyh <kwonyong6@gmail.com>
2022-12-18 23:04:25 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://localhost:8080/',
+      target: process.env.REACT_APP_HOST,
       changeOrigin: true,
     }),
   );
